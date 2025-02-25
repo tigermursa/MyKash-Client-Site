5e825b7c5dba56dc604c22436ebe7c99c4416d6a
Mursalin <mursalin2311@gmail.com>
2025-02-26 02:50:15 +0600

    just installed react vite tailind 4 and react dom

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center h-screen items-center text-5xl text-red-400 bg-black">
+      Hello New Project
+    </div>
+  );
+};
+
+export default App;
