a77ed14a9ff9397e4f42fae7648572e42307845a
Mursalin <mursalin2311@gmail.com>
2025-02-27 10:06:46 +0600

    Updated form design

--- src/components/authForm/register.tsx
+++ src/components/authForm/register.tsx
@@ -2,9 +2,20 @@ import { AuthForm } from "./auth-form";
 
 export default function SignUp() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4  ">
-      <div className="z-20">
-        <AuthForm type="sign-up" />
+    <div className="min-h-screen flex">
+      {/* Left-side image only visible on large screens */}
+      <div className="hidden lg:block lg:w-1/2">
+        <img
+          src="https://res.cloudinary.com/dvwmhlyd6/image/upload/f_auto,q_auto/v1740628950/adef3d98-6e24-4353-bc0d-dbb3cfeb9aff_njffuh.png"
+          alt="Sign Up Background"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      {/* Right-side form */}
+      <div className="flex items-center justify-center w-full lg:w-1/2 p-4">
+        <div className="z-20">
+          <AuthForm type="sign-up" />
+        </div>
       </div>
     </div>
   );
